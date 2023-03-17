70b2a7e6f73acbc4484c5168963f6b8c0eb3b9d0
Lafdi Yahya <yahya.lafdi@edu.uiz.ac.ma>
2023-03-17 21:18:26 +0100

    updating home banner

--- src/app/components/main-slider/main-slider.component.ts
+++ src/app/components/main-slider/main-slider.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-main-slider',
   templateUrl: './main-slider.component.html',
-  styleUrls: ['./main-slider.component.css']
+  styleUrls: ['./main-slider.component.css'],
+  encapsulation: ViewEncapsulation.None
+  
 })
 export class MainSliderComponent {
 
