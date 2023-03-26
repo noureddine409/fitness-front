57ad179eda455b7f3db2d0a0f672ba151786be8f
noureddine <noureddinelachgar9@gmail.com>
2023-03-26 03:14:34 +0000

    display authenticated user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {AppUser} from "../../models/user.model";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() currentUser!: AppUser | null ;
 
 }
