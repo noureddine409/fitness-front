dc787be6b897b44409ecca037648d2dc7608546e
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-04-04 15:04:37 +0200

    our-services-items.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,10 +22,12 @@ import {ResetPasswordComponent} from "./components/reset-password/reset-password
 import {ErrorComponent} from "./components/error/error.component";
 import {ForgetPasswordVerifyComponent} from "./components/forget-password-verify/forget-password-verify.component";
 import {ResetPasswordGuard} from "./guards/reset-password/reset-password.guard";
+import {OurServicesComponent} from "./components/our-services/our-services.component";
+import {OurServicesItemsComponent} from "./components/our-services-items/our-services-items.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent , canActivate: [HomeGuard]},
+  { path: 'home', component: HomeComponent, canActivate: [HomeGuard] },
   { path: 'our-programs', component: OurProgramsComponent, canActivate: [HomeGuard]},
   { path: 'our-trainers', component: OurTrainersComponent, canActivate: [HomeGuard] },
   { path: 'blogs', component: BlogsComponent, canActivate: [HomeGuard] },
@@ -41,6 +43,8 @@ const routes: Routes = [
   { path: 'forget-password-verify', component: ForgetPasswordVerifyComponent},
   { path: 'reset-password', component: ResetPasswordComponent, canActivate: [ResetPasswordGuard]},
   { path: 'error-404', component: ErrorComponent},
+
+
 ];
 
 @NgModule({
