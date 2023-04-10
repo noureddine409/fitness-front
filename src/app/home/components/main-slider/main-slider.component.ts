edb64ce873134ab9feb33d14a92b3c5c3897eedb
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-04-10 23:47:20 +0200

    some text changes

--- src/app/home/components/main-slider/main-slider.component.ts
+++ src/app/home/components/main-slider/main-slider.component.ts
@@ -1,12 +1,18 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main-slider',
   templateUrl: './main-slider.component.html',
   styleUrls: ['./main-slider.component.css'],
   encapsulation: ViewEncapsulation.None
-  
+
 })
 export class MainSliderComponent {
+  constructor(private router:Router) {
+  }
+  goToOtherComponent(url:string) {
+    this.router.navigate([url]);
+  }
 
 }
