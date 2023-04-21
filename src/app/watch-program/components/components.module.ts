77eae76e54539d1ba7baf9f2f9e810cbe92da4b8
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-04-21 14:54:21 +0200

    watch-program-comments components created

--- src/app/watch-program/components/components.module.ts
+++ src/app/watch-program/components/components.module.ts
@@ -2,12 +2,14 @@ import {NgModule, Type} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { ProgramPanelComponent } from './program-panel/program-panel.component';
 import { ProgramVideoComponent } from './program-video/program-video.component';
+import { ProgramCommentComponent } from './program-comment/program-comment.component';
+import { ProgramCreateCommentComponent } from './program-create-comment/program-create-comment.component';
 
 
-const watchProgramsComponents: Type<any>[] = [ProgramPanelComponent,ProgramVideoComponent];
+const watchProgramsComponents: Type<any>[] = [ProgramPanelComponent,ProgramVideoComponent,ProgramCommentComponent, ProgramCreateCommentComponent];
 
 @NgModule({
-  declarations: [...watchProgramsComponents, ProgramPanelComponent, ProgramVideoComponent],
+  declarations: [...watchProgramsComponents],
   imports: [
     CommonModule
   ],
---
+++ src/app/watch-program/components/program-create-comment/program-create-comment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-program-create-comment',
+  templateUrl: './program-create-comment.component.html',
+  styleUrls: ['./program-create-comment.component.css']
+})
+export class ProgramCreateCommentComponent {
+
+}
